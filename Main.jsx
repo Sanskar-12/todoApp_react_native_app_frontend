0357ead718b662a2933f018711b5ec5ddff0354a
Sanskar-12 <sanskarv2004@gmail.com>
2024-03-29 13:36:38 +0530

    frontend

--- Main.jsx
+++ Main.jsx
@@ -1,4 +1,3 @@
-import { View, Text } from "react-native";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -6,6 +5,8 @@ import Home from "./screens/Home";
 import Login from "./screens/Login";
 import Footer from "./components/Footer";
 import Profile from "./screens/Profile";
+import Register from "./screens/Register";
+import CameraComponent from "./screens/CameraComponent";
 
 const Stack = createNativeStackNavigator();
 
@@ -19,11 +20,26 @@ const Main = () => {
           options={{ headerShown: false }}
         />
         <Stack.Screen
+          name="register"
+          component={Register}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
           name="home"
           component={Home}
           options={{ headerShown: false }}
         />
-        <Stack.Screen name="profile" component={Profile} />
+        <Stack.Screen
+          name="profile"
+          component={Profile}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="camera"
+          component={CameraComponent}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
       <Footer />
     </NavigationContainer>
---
+++ screens/Register.jsx
@@ -0,0 +1,115 @@
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet,
+} from "react-native";
+
+import { Avatar, Button } from "react-native-paper";
+import React, { useEffect, useState } from "react";
+
+const Register = ({ navigation, route }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [avatar, setAvatar] = useState(null);
+
+  const handleImage = () => {
+    navigation.navigate("camera");
+  };
+  const registerHandler = () => {
+    console.log("Register");
+  };
+
+  useEffect(() => {
+    if (route?.params) {
+      console.log(route.params.image);
+
+      if (route?.params?.image) {
+        setAvatar(route?.params?.image);
+      }
+    }
+  }, [route]);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Avatar.Image
+        size={100}
+        source={{ uri: avatar ? avatar : null }}
+        style={{
+          backgroundColor: "#900",
+        }}
+      />
+      <TouchableOpacity onPress={handleImage}>
+        <Text style={{ color: "#900" }}>Change Photo</Text>
+      </TouchableOpacity>
+      <View style={{ width: "70%" }}>
+        <TextInput
+          style={styles.input}
+          placeholder="Name"
+          value={name}
+          onChangeText={setName}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+        />
+        <TextInput
+          secureTextEntry
+          style={styles.input}
+          placeholder="Password"
+          value={password}
+          onChangeText={setPassword}
+        />
+      </View>
+      <Button
+        disabled={!email || !password}
+        style={styles.btn}
+        onPress={registerHandler}
+      >
+        <Text style={{ color: "#fff" }}>SIGN UP</Text>
+      </Button>
+      <TouchableOpacity onPress={() => navigation.navigate("login")}>
+        <Text
+          style={{
+            color: "#900",
+            height: 30,
+            margin: 20,
+          }}
+        >
+          Have an Account, Login
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Register;
+
+const styles = StyleSheet.create({
+  input: {
+    backgroundColor: "#fff",
+    borderWidth: 1,
+    borderColor: "#b5b5b5",
+    padding: 10,
+    paddingLeft: 15,
+    borderRadius: 5,
+    marginVertical: 15,
+    fontSize: 15,
+  },
+  btn: {
+    backgroundColor: "#900",
+    padding: 5,
+    width: "70%",
+  },
+});
