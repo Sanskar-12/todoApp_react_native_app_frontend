c017add64d0853d72cfad08f0bf5d0b93e563ef5
Sanskar-12 <sanskarv2004@gmail.com>
2024-03-30 06:53:08 +0530

    frontend

---
+++ redux/action.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+const server = "https://react-native-todo-app-server.onrender.com";
+
+export const login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({ type: "loginRequest" });
+
+    const { data } = await axios.post(
+      `${server}/api/v1/login`,
+      { email, password },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true,
+      }
+    );
+
+    dispatch({ type: "loginSuccess", payload: data });
+  } catch (error) {
+    dispatch({ type: "loginFail", payload: error.response.data.message });
+  }
+};
+
+export const getMyProfile = () => async (dispatch) => {
+  try {
+    dispatch({ type: "loadUserRequest" });
+
+    const { data } = await axios.get(`${server}/api/v1/me`, {
+      withCredentials: true,
+    });
+
+    dispatch({ type: "loadUserSuccess", payload: data });
+  } catch (error) {
+    dispatch({ type: "loadUserFail", payload: error.response.data.message });
+  }
+};
